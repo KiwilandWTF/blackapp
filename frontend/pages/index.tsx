66f901b39865aff5b728adc6d082d8a92efb4dd2
Juan Cedeno (MBPro) <henixceo@gmail.com>
2022-03-11 22:36:57 -0500

    quick draft prototype ui

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,20 +9,90 @@ const Home: NextPage = () => {
   const { user, error, isLoading } = useUser();
   // 
 
+
+  let logo = "https://github.com/KiwilandWTF/blackapp/raw/main/frontend/public/hynix-logo-2x.png";
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Blackapp - Home</title>
       </Head>
-      <main className={styles.main}>
-        <h1 className="text-5xl bold text-stone-700">
-          Blackbird
-        </h1>
 
-        {authButton(user)}
+      <body>
+
+        {/* !--Navbar goes here -->*/}
+        <div className="bg-white shadow-lg">
+          <div className="max-w-6xl mx-auto px-4">
 
 
-      </main>
+            <nav className="bg-white">
+              <ul className="flex justify-self-stretch">
+                <li className="mr-6">
+                  <a className="text-blue-500 hover:text-blue-800" href="#">Home</a>
+                </li>
+                <li className="mr-6">
+                  <a className="text-blue-500 hover:text-blue-800" href="#">Pricing</a>
+                </li>
+                <li className="mr-6">
+                  <a className="text-blue-500 hover:text-blue-800" href="#">Discord</a>
+                </li>
+                <li className="mr-6">
+                  <a className="text-gray-400 cursor-not-allowed" href="#">Blackbird Terminal</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        {/* !--Main content goes here -->*/}
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="flex flex-col">
+            <div>
+              <p>
+                <br></br>
+              </p>
+            </div>
+            <div className="flex-1 flex-col px-4">
+              <div className="flex justify-center">
+                <img className="h-16 w-16" src={logo} alt="hynix logo" />
+              </div>
+              <div className="flex justify-center">
+                <h1 className="text-3xl font-bold text-center">Blackapp</h1>
+              </div>
+              <div className="flex justify-center">
+                <p className="text-center">
+                  A simple, minimalistic, and modern web application for managing your blackbird instances.
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <p className="text-center">
+                  <a className="text-blue-500 hover:text-blue-800" href="#">Learn more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <main>
+
+        </main>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </body>
 
       <footer className={styles.footer}>
         <a
@@ -37,29 +107,6 @@ const Home: NextPage = () => {
   )
 }
 
-function authButton(user: any) {
-
-  if (user === undefined || user === null) return (<div>
-    <a href="/api/auth/login" className='bold cursor-all-scroll'>Sign in</a>
-  </div>);
-
-
-  let logout = (
-    <div>
-      <h2>Welcome <a className="text-red-600 bold ">{user.name}</a>!</h2>
-
-      <img src={user.picture} alt="Profile" />
-      <p>
-        <br></br>
-        <a href="/api/auth/logout" className='text-gray-700 bold underline'>Logout</a>
-      </p>
-    </div>
-  );
-
-  return (<div>
-    {logout}
-  </div>);
-}
 
 
 
